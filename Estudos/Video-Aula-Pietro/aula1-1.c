a8b3fafa147dbb48f1aad0d47dbbab537d050fee
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-04 13:57:26 -0300

    Create aula1-1.c

---
+++ Estudos/Video-Aula-Pietro/aula1-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //Informando ao programa que iremos aprroveitar dessa biblioteca (na verdade é um programa pré-programado)
+#include <stdlib.h> // Outra biblioteca, faz com que o programa pause no final, não finalizando, aguardando o usuário.
+
+int main() { //Corpo principal, função principal
+    printf("\nBahia eh o mundo!\n\n");
+
+    system("pause");
+}
