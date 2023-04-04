8785e78ca15d5eb8f1e401637beb0e57db717cc2
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-04 13:26:40 -0300

    #include <stdio.h>  // Ver o que está dando de errado - ECLARAÇÃO DE FUNÇÕES ESTRITAS E BÁSICAS, (PRINTF E SCANF)
        int main()
        {
    
            printf("Olá mundo!");
            return 0;
        }

---
+++ Anotações-Aula01/Aula01.c
@@ -0,0 +1,8 @@
+    #include <stdio.h>  // Ver o que está dando de errado - ECLARAÇÃO DE FUNÇÕES ESTRITAS E BÁSICAS, (PRINTF E SCANF)
+
+    int main() 
+    {
+
+        printf("Olá mundo!");
+        return 0;
+    }
--- Anotações/Aula01.c
+++
@@ -1,8 +0,0 @@
-    #include <stdio.h>  // Ver o que está dando de errado - ECLARAÇÃO DE FUNÇÕES ESTRITAS E BÁSICAS, (PRINTF E SCANF)
-
-    int main() 
-    {
-
-        printf("Olá mundo!");
-        return 0;
-    }
