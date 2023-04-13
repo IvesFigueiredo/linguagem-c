3df014825d12257bf68df6377640c94e10722b03
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-13 16:06:48 -0300

    Update 1QUestao_Logica_bool.c

--- Estudos/Listas_de_exercicios/Lista_02/1QUestao_Logica_bool.c
+++ Estudos/Listas_de_exercicios/Lista_02/1QUestao_Logica_bool.c
@@ -4,29 +4,36 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main() {
+int main(void) {
 
-    bool verif;
-    int num1, num2, num3;
+        bool verif;
+        int num1, num2, num3;
+        char retorno;
 
-    printf("Informe o primeiro numero:\n");
-    scanf("%d", &num1);
+    do{
 
-    printf("Informe o segundo numero:\n");
-    scanf("%d", &num2);
+system("cls");
+        printf("Informe o primeiro numero:\n");
+        scanf("%d", &num1);
 
-    printf("Informe o terceiro numero:\n");
-    scanf("%d", &num3);
+        printf("Informe o segundo numero:\n");
+        scanf("%d", &num2);
 
-    verif = num1+num2 < num3;
+        printf("Informe o terceiro numero:\n");
+        scanf("%d", &num3);
 
-    printf("Verificador: %d.\n", verif);
+        verif = num1+num2 < num3;
 
-    if(verif == 1) {
-        printf("A soma dos dois primeiros numeros eh menor que o terceiro numero informado.\n");
-    } else {
-        printf("A soma dos dois primeiros numeros eh maior que o terceiro numero informado.\n");
-    }
+        printf("Verificador: %d.\n", verif);
 
-    system("pause");
+        if(verif == 1) {
+            printf("A soma dos dois primeiros numeros eh menor que o terceiro numero informado.\n");
+        } else {
+            printf("A soma dos dois primeiros numeros eh maior que o terceiro numero informado.\n");
+        }
+
+        printf("Deseja realizar outra operação? [ s - Sim | n - Não ]\n");
+        scanf(" %c", &retorno);
+    }while (retorno != 'n' && retorno != 'N');
+
 }
