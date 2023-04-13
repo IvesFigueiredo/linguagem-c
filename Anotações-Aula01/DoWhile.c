a0fdd37151889e2d2ff4b87466f8848438d34a8e
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-12 21:11:48 -0300

    Create DoWhile.c

---
+++ Anotações-Aula01/DoWhile.c
@@ -0,0 +1,19 @@
+// Como implementar o Do While.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i=0;
+system("cls");
+
+    do {
+        printf("Aprendendo DO While.\n");
+        i++;
+    } while (i <5);
+
+system("pause");
+
+    return 0;
+}
