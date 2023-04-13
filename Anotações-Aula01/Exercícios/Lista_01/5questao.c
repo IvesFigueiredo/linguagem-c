6e86e825348656628f4843bb0c35fb199ed759b5
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-13 15:23:38 -0300

    Create 5questao.c

---
+++ Anotações-Aula01/Exercícios/Lista_01/5questao.c
@@ -0,0 +1,45 @@
+//  Lista 01 - Questão 05.
+//  Nome: Ives Gabriel Figueiredo Santos Pinto.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main () {
+
+    int  time1, time2;
+    char  nomTim1[50], nomTim2[50];
+    char retorno;
+
+    do {
+system("cls");
+
+        printf("Informe o nome do 1º time: ");
+        scanf("%s", nomTim1);
+        printf("Informe o nome do 2º time: ");
+        scanf("%s", nomTim2);
+
+        do {
+        printf("Quantos gols o %s realizou no jogo?\n", nomTim1);
+        scanf("%d", &time1);
+        }while(time1 < 0); 
+        do {
+        printf("Quantos gols o %s realizou no jogo?\n", nomTim2);
+        scanf("%d", &time2);
+        } while (time2 < 0);
+        if (time1 > time2) {
+            printf("O %s venceu!\n", nomTim1);
+            printf("Placar: %s - %d x %d - %s.\n", nomTim1, time1, time2, nomTim2);
+        } else if ( time1 < time2) {
+            printf("O %s venceu!\n", nomTim2);
+            printf("Placar: %s - %d x %d - %s.\n", nomTim1, time1, time2, nomTim2);
+        }else {
+            printf("Houve empate!\nPlacar: %s - %d x %d - %s.\n", nomTim1, time1, time2, nomTim2);
+        }
+
+    printf("Deseja realizar outra operação? [ s - Sim | n - Não ]\n");
+    scanf(" %c", &retorno);
+    } while ( retorno != 'n' && retorno != 'N');
+
+return 0;
+}
