549c075d6a8e40822484cca4497f64fe493f18cf
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-12 21:12:08 -0300

    Create for.c

---
+++ Anotações-Aula01/for.c
@@ -0,0 +1,18 @@
+// Como implementar o For.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int i;
+    system("cls");
+
+        for (i=0; i< 5; i++) {
+            printf("Aprendendo FOR.\n");
+        }
+        return 0;
+        system("pause");
+
+return 0;
+}
