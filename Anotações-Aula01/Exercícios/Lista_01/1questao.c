5c5583746b760d7b89d0e7b022967e9fe142635a
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-13 16:06:26 -0300

    Create 1questao.c

---
+++ Anotações-Aula01/Exercícios/Lista_01/1questao.c
@@ -0,0 +1,39 @@
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void) {
+
+        bool verif;
+        int num1, num2, num3;
+        char retorno;
+
+    do{
+
+system("cls");
+        printf("Informe o 1º número: ");
+        scanf("%d", &num1);
+
+        printf("Informe o 2º número: ");
+        scanf("%d", &num2);
+
+        printf("Informe o 3º número: ");
+        scanf("%d", &num3);
+
+        verif = num1+num2 < num3;
+
+        printf("Verificador: %d.\n", verif);
+
+        if(verif == 1) {
+            printf("A soma dos dois primeiros números é menor que o terceiro número informado.\n");
+        } else {
+            printf("A soma dos dois primeiros números é maior que o terceiro número informado.\n");
+        }
+
+        printf("Deseja realizar outra operação? [ s - Sim | n - Não ]\n");
+        scanf(" %c", &retorno);
+    }while (retorno != 'n' && retorno != 'N');
+
+}
