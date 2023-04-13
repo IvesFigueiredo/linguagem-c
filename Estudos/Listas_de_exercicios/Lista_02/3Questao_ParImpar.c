5dfc6886deac1ce3fb690dc42a104ae49499b45e
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-13 16:06:51 -0300

    Update 3Questao_ParImpar.c

--- Estudos/Listas_de_exercicios/Lista_02/3Questao_ParImpar.c
+++ Estudos/Listas_de_exercicios/Lista_02/3Questao_ParImpar.c
@@ -6,16 +6,23 @@
 int main() {
 
     int num1;
+    char retorno;
 
-    printf("Para verificar se o número é par ou ímpar.\nInforme um número:\n");
-    scanf("%d", &num1);
+    do {
+system("cls");
+        printf("Para verificar se o número é par ou ímpar.\nInforme um número:\n");
+        scanf("%d", &num1);
 
-    if ( num1%2 ==0 && num1 !=0) {
-        printf("É par.\n");
-    } else if (num1 == 0) {
-        printf("É nulo.\n");
-    } else {
-        printf("É impar.\n");
-    }
-    system("pause");
+        if ( num1%2 ==0 && num1 !=0) {
+            printf("É par.\n");
+        } else if (num1 == 0) {
+            printf("É nulo.\n");
+        } else {
+            printf("É impar.\n");
+        }
+
+        printf("Deseja realizar outra operação? [ s - Sim | n - Não ]\n");
+        scanf(" %c", &retorno);
+    }while (retorno != 'n' && retorno != 'N');
+return 0;
 }
