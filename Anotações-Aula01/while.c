12779f045f482c5045287aac08caec7981e2899c
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-12 21:12:23 -0300

    Create while.c

---
+++ Anotações-Aula01/while.c
@@ -0,0 +1,20 @@
+// Como implementar o While.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    system("cls");
+
+    int i=0;
+
+    while(i < 5) {
+        printf("Aprendendo WHILE.\n");
+        i++;
+    }
+
+    system("pause");
+
+    return 0;
+}
