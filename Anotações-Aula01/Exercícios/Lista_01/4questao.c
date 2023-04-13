f83339dd42aa5e05c103f3779a8c10b34bf0840c
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-12 21:11:53 -0300

    Create 4questao.c

---
+++ Anotações-Aula01/Exercícios/Lista_01/4questao.c
@@ -0,0 +1,58 @@
+// 4° Questão da 1° Lista.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    float sal, novoSal, difSal;
+    int  codigo;
+    char retorno;
+system("cls");
+
+    do {
+        printf("\t\tSistema para reajuste de salário.\n\nInforme o seu salário atual: ");
+        scanf("%f", &sal);
+        printf("Informe o código da função que você exerce: ");
+        scanf("%d", &codigo);
+
+        switch(codigo) {
+
+            case 310 :
+
+                novoSal = sal*1.05;
+                difSal = novoSal - sal;
+                printf("O seu novo salário é R$ %.2f\nA diferença do salário antigo com o novo é de R$ %.2f\n", novoSal, difSal);
+
+            break;
+
+            case 456 :
+
+                novoSal = sal*1.075;
+                difSal = novoSal-sal;
+                printf("O seu novo salário é R$ %.2f\nA diferença do salário antigo com o novo é de R$ %.2f\n", novoSal, difSal);
+        
+            break;
+
+            case 885 :
+
+                novoSal = sal*1.1;
+                difSal = novoSal-sal;
+                printf("O seu novo salário é R$ %.2f\nA diferença do salário antigo com o novo é de R$ %.2f\n", novoSal, difSal);
+
+            break;
+
+            default :
+
+                novoSal = sal*1.15;
+                difSal = novoSal-sal;
+                printf("O seu novo salário é R$ %.2f\nA diferença do salário antigo com o novo é de R$ %.2f\n", novoSal, difSal);
+            break;
+        }
+        printf("\nDeseja preencher novamente? [s - sim | n - não]\n");
+        scanf("%c", &retorno);
+    } while (retorno != 'n');
+
+
+return 0;
+}
