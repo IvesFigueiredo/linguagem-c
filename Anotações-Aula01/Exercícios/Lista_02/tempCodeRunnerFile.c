2396b43621ffdb2dff5f381712b97e9503b67ad5
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-14 23:33:23 -0300

    Create tempCodeRunnerFile.c

---
+++ Anotações-Aula01/Exercícios/Lista_02/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+void fimPrograma()
+{
+    printf("\t\t\t________________________________________\n\t\t\t|\t\t\t\t\t|\n\t\t\t|\t   Direitos Reservados\t\t|\n\t\t\t|\t₢ Ives Figueiredo Pinto\t\t|\n\t\t\t|\t\tATÉ LOGO!\t\t|\n\t\t\t|_______________________________________|\n\n");
+}
