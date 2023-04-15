3025fc5140f7389dd6ba8f1f06082b0d714134fe
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-14 23:31:36 -0300

    Update 1questao_tabuada.c

--- Anotações-Aula01/Exercícios/Lista_02/1questao_tabuada.c
+++ Anotações-Aula01/Exercícios/Lista_02/1questao_tabuada.c
@@ -1,31 +1,42 @@
-//Nome: Ives Gabriel Figueiredo Santos Pinto
-//Questão 01 Lista 02
+// Nome: Ives Gabriel Figueiredo Santos Pinto
+// Questão 01 Lista 02
 
 #include <stdio.h>
 #include <stdlib.h>
 
-float multiplica (float num1, int multiplicador){
-   float resultado = num1*multiplicador;
+float multiplica(float num1, int multiplicador)
+{
+    float resultado = num1 * multiplicador;
     return resultado;
 }
 
-int main(void) {
+int main(void)
+{
 
     int i;
     float n1;
     char retorno;
 
-    do{
-system("cls");
+    do
+    {
+        system("cls");
 
         printf("\t\t\tPrograma para calcular a tabuada.\n\nInforme um número: ");
         scanf("%f", &n1);
 
-        for(i=1; i<=10; i++){
+        for (i = 1; i <= 10; i++)
+        {
             printf("%.1f x %d = %.1f\n", n1, i, multiplica(n1, i));
         }
 
         printf("Deseja realizar outra operação? [ s - Sim | n - Não ]\n");
         scanf(" %c", &retorno);
     } while (retorno != 'n' && retorno != 'N');
+
+    fimPrograma();
+}
+
+void fimPrograma()
+{
+    printf("\t\t\t________________________________________\n\t\t\t|\t\t\t\t\t|\n\t\t\t|\t   Direitos Reservados\t\t|\n\t\t\t|\t₢ Ives Figueiredo Pinto\t\t|\n\t\t\t|\t\tATÉ LOGO!\t\t|\n\t\t\t|_______________________________________|\n\n");
 }
