4d9bf486a638d25afb5cf1da75797bb399dd8d36
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-05-03 19:41:01 -0300

    Update 3questao.c

--- Anotações-Aula01/Exercícios/Lista_05/3questao.c
+++ Anotações-Aula01/Exercícios/Lista_05/3questao.c
@@ -1,4 +1,4 @@
-/*Nome: Ives Gabriel Figueiredo Santos Pinto
+/*Nome: Ives Gabriel Figueiredo Santos Pinto  
 Assunto: Lista 05, Questão 03*/
 
 #include <stdio.h>
@@ -6,10 +6,10 @@ Assunto: Lista 05, Questão 03*/
 
 int mdc(int n1, int n2)
 {
-    if (n2 == 0)
+    if (n2 == 0) // Pois sabemos que o maior número que divide o n1 é o próprio n1 e também qualquer número divide o 0. Logo mdc(a, 0) = 0
     {
-        return n1;
-    } else
+        return n1; 
+    } else  // mdc(a, b) --> mdc(b, %a) caso, o %a = 0, será a mesma coisa que a condição 01, mdc(b, 0) = b, também, caso o n1 > n2, no calculo ele troca de posição. 
     {
         return (mdc (n2, n1 % n2 ));
     }
