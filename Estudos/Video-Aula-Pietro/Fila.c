851a2615f213de070ad6e088f63d058b3c295ded
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-05-24 20:56:01 -0300

    Update Fila.c

--- Estudos/Video-Aula-Pietro/Fila.c
+++ Estudos/Video-Aula-Pietro/Fila.c
@@ -28,5 +28,18 @@ int main(void)
     f.fim++;
 
     // remover da fila
-    printf("");
+    printf("Elemento removido da fila: %d\n", f.fila[0]);
+    f.fila[0] = f.fila[1];  //realiza o deslocamento da segunda posição para a primeira posição
+    f.fila[1] = f.fila[2]; //realiza a deslocamento da 3° posição para a 2° posição
+    f.fim--;
+
+    // removendo o segundo elemento fila
+    printf("Elemento removido da fila: %d\n", f.fila[0]);
+    f.fila[0] = f.fila[1]; //realiza o deslocamento
+    f.fila[1] = f.fila[2];
+    f.fim--;
+
+    printf("Elemento removido da fila: %d\n", f.fila[0]);
+    f.fila[0] = f.fila[1]; // realiza o deslocamento
+    f.fim--;
 }
